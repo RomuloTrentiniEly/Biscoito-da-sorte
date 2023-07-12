ccf037f5b09202c73c4763934697d474ed2889ae
RomuloTrentiniEly <romuloely23@gmail.com>
2023-07-12 10:10:06 -0300

    first commit

---
+++ meuestudo/src/App.js
@@ -0,0 +1,64 @@
+
+import React, { Component } from 'react';
+import './App.css'
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = { 
+      textoFrase: ''
+    };
+      this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+      this.frase = ['Siga os bons e aprenda com eles.', 'O bom-senso vale mais do que muito conhecimento.', 
+      'O riso é a menor distância entre duas pessoas.', 
+      'Deixe de lado as preocupações e seja feliz.',
+      'Realize o óbvio, pense no improvável e conquiste o impossível.',
+      'Acredite em milagres, mas não dependa deles.',
+      'A maior barreira para o sucesso é o medo do fracasso.'];
+  }
+      
+      quebraBiscoito(){
+        let state = this.state
+        let numeroAleatorio = Math.floor(Math.random() * this.frase.length);
+        state.textoFrase = this.frase[numeroAleatorio]
+        this.setState(state);
+     }
+   
+
+    
+
+  
+
+  
+  render(){
+
+    return(
+      
+      <div className='container'>
+      <img src={require('./img/biscoito.png')} className='img'/>
+      
+      <Botao nome='Abrir Biscoito' acaoBtn={this.quebraBiscoito}/>
+      <h3 className='textoFrase'>{this.state.textoFrase}</h3>
+        </div>
+            
+    );
+  }
+}   
+
+
+class Botao extends Component{
+  render() {
+    return(
+
+      <div>
+        <button onClick={this.props.acaoBtn}> {this.props.nome}</button>
+      </div>
+
+    );
+  }
+}
+
+
+export default App;
